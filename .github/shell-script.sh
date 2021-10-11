d25f59e9d86c75fb4cfd8735d780c020951b280d
sedosona <sedosona@kali.sedosona>
2021-10-11 12:27:15 +0200

    aa"

---
+++ .github/shell-script.sh
@@ -0,0 +1,15 @@
+IFS=":"
+while read -r  f1 f2 f3
+do
+num1=${f1}
+num2=${f2}
+result=${f3}
+sc_result=$(./sum.sh ${num1} ${num2})
+if [ ${sc_result} -eq ${result} ]
+then
+	echo "case passed"
+else
+	echo "failed"
+	exit 101
+fi
+done < 'dataset'
