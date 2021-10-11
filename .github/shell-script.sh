7bbd4d77dee1bb6cca8ccc0318ef0a34860f6c7c
sedosona <sedosona@kali.sedosona>
2021-10-11 12:34:53 +0200

    tt43

--- .github/shell-script.sh
+++ .github/shell-script.sh
@@ -3,9 +3,10 @@ while read -r  f1 f2 f3
 do
 num1=${f1}
 num2=${f2}
+echo ${num1}
 result=${f3}
 sc_result=$(./sum.sh ${num1} ${num2})
-if [ ${sc_result} -eq ${result} ]
+if [ ${sc_result} -eq $((result+1)) ]
 then
 	echo "case passed"
 else
